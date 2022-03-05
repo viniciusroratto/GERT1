d09da07b6558a87f86257ab8e823ee895505f256
viniciusroratto <vini.jornal@gmail.com>
2022-03-05 17:29:09 -0300

    classe SES_MGR"

--- session_manager.py
+++ session_manager.py
@@ -1,7 +1,32 @@
 from easysnmp import Session
 
+class SES_MGR():
+    test = "HALO"
+    def __init__(self, hostname = u'localhost',
+                       security_level = 'authNoPriv',
+                       security_username = 'SNMPUSER',
+                       auth_protocol = 'DEFAULT',
+                       auth_password = '123456789DEZ',
+                       privacy_protocol = u'DEFAULT',
+                       privacy_password = '123456789DEZ'):
+
+        self.session = Session(hostname = hostname,
+                       version = 3,
+                       security_level = security_level,
+                       security_username = security_username,
+                       auth_password = '123456789DEZ',
+                       auth_protocol = auth_protocol,
+                       privacy_protocol = privacy_protocol,
+                       privacy_password = privacy_password)
+
+    def get(self, oid):
+        return self.session.get(oid)
+
+
+
+
 # Create an SNMP session to be used for all our requests
-session = Session(hostname='localhost', community='public', version=2)
+session = SES_MGR()
 
 # You may retrieve an individual OID using an SNMP GET
 location = session.get('sysLocation.0')
@@ -15,7 +40,7 @@ contact = session.get(('sysContact', '0'))
 description = session.get('.1.3.6.1.2.1.1.1.0')
 
 # Set a variable using an SNMP SET
-session.set('sysLocation.0', 'The SNMP Lab')
+#session.set('sysLocation.0', 'The SNMP Lab')
 
 # Perform an SNMP walk
 system_items = session.walk('system')
@@ -24,6 +49,8 @@ system_items = session.walk('system')
 # but also has several extended attributes with SNMP-specific information
 for item in system_items:
     print('{oid}.{oid_index} {snmp_type} = {value}'.format(oid=item.oid,oid_index=item.oid_index, snmp_type=item.snmp_type, value=item.value))
+
+
 
 
 
