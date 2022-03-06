3509df0f44d9f3cbb17219b9fc64ba47cae5b1f3
Gerencia de Redes <vicentelessa15@hotmail.com>
2022-03-06 13:49:25 -0300

    fixed git_example_3.py to work for our previously defined users in AP3

--- git_example_3.py
+++ git_example_3.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+# !/usr/local/bin/python3
 
 '''
 snmp-server view SV_EASYSNMP interfaces included
@@ -16,9 +16,26 @@ snmp-server user EASYSNMP SG_EASYSNMP v3 auth sha AUTHPASS priv aes 128 PRIVPASS
 
 from easysnmp import Session
 
-session3 = Session(hostname='192.168.2.72', version=3,
-security_level="auth_with_privacy", security_username="EASYSNMP",
-auth_protocol="SHA", auth_password="AUTHPASS",
-privacy_protocol="AES", privacy_password="PRIVPASS")
 
-session3.walk("1.3.6.1.4.1.9.9.166.1.15.1.1.2")
+session = Session(hostname='192.168.0.107', version=3, security_level="auth_with_privacy", security_username="MD5DESUser",
+auth_protocol="MD5", auth_password="The Net-SNMP Demo Password",
+privacy_protocol="DES", privacy_password="The Net-SNMP Demo Password")
+
+#session3.walk("1.3.6.1.4.1.9.9.166.1.15.1.1.2")
+
+name = session.get('sysName.0')
+uptime = session.get('sysUpTimeInstance')
+ifnumber = session.get('ifNumber.0')
+services = session.get('sysServices.0')
+#walk = []
+#walk = session.walk()
+# processos = session.get('sysUpTimeInstance')
+# Agora basta imprimir a variavel name
+print (name,'\n')
+print ("name.value",name.value,"\n")
+print ('name.snmp_type',name.snmp_type,'\n')
+print ('uptime: ',uptime,'\n')
+print ('services: ',services,'\n')
+print ('ifnumber: ',ifnumber,'\n')
+print ('ifnumber.value: ',int(ifnumber.value),'\n')
+#print ('walk: ',walk,'\n')
